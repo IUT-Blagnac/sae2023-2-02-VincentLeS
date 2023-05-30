3691531b9305e851cf169034116ba674ad95a617
VincentLeS <vincent.barette@outlook.fr>
2023-05-30 15:47:26 +0200

    Commit Meilleurs

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
         String texte = "exemple de texte a classer";
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
-        System.out.println(ExerciceNeutre.solution(texte, ordre));
+        System.out.println(SimpliciteMeilleur.solution(texte, ordre));
+        System.out.println(EfficaciteMeilleur.solution(texte, ordre));
+        System.out.println(SobrieteMeilleur.solution(texte, ordre));
         // output: [classer, texte, exemple, a, de]
     }
 }
---
+++ src/main/java/exercice/SobrieteMeilleur.java
@@ -0,0 +1,33 @@
+package exercice;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SobrieteMeilleur {
+    public static List<String> solution(String str, List<Character> ordre) {
+        // La version du code trie de façon plus sobre les mots.
+        // En effet, une seule liste est utilisée, et les mots sont déplacés au début de
+        // la liste au fur et à mesure qu'ils sont trouvés.
+
+        List<String> result = new ArrayList<String>(Arrays.asList(str.split(" ")));
+
+        // On parcourt l'ordre de tri à l'envers
+        for (int i = ordre.size() - 1; i >= 0; i--) {
+            // On recherche les mots commençant par la lettre courante
+            for (int j = 0; j < result.size(); j++) {
+                if (result.get(j).charAt(0) == ordre.get(i)) {
+                    // ... on les déplace au début de la liste ...
+                    deplacerElement(result, j, 0);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void deplacerElement(List<String> liste, int indexBase, int indexDestination) {
+        String elt = liste.get(indexBase);
+        liste.remove(indexBase);
+        liste.add(indexDestination, elt);
+    }
+}
