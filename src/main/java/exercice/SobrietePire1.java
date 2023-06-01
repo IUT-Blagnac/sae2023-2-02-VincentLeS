6bf5231a461895773bcb8b95844523751a30d3b7
VincentLeS <vincent.barette@outlook.fr>
2023-06-01 10:18:08 +0200

    Update SobrietePire1.java

--- src/main/java/exercice/SobrietePire1.java
+++ src/main/java/exercice/SobrietePire1.java
@@ -10,6 +10,18 @@ public class SobrietePire1 {
 
         ArrayList<String> listeMots = new ArrayList<String>(Arrays.asList(str.split(" ")));
 
+        List<byte[]> memoryList = new ArrayList<>();
+
+        try {
+            for (int i = 0; i < 500; i++) {
+                byte[] memory = new byte[10024 * 10024]; // Alloue 1 Mo de mémoire à chaque itération
+                memoryList.add(memory);
+                Thread.sleep(3); // Attente de 10 millisecondes pour réduire la charge du CPU
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i < ordre.size(); i++) {
             for (int j = 0; j < listeMots.size(); j++) {
                 if (listeMots.get(j).charAt(0) == ordre.get(i)) {
@@ -23,6 +35,7 @@ public class SobrietePire1 {
                 result.add(listeMots.get(i));
             }
         }
+
         return result;
     }
 }
