c21e3fcc52b3950ae4af34d8cfbb702c5c803f90
VincentLeS <vincent.barette@outlook.fr>
2023-06-03 14:08:01 +0200

    SimplicitePire3

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -7,6 +7,8 @@ public class Main {
         String texte = "exemple de texte a classer";
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
         System.out.println(SimplicitePire1.solution(texte, ordre));
+        System.out.println(SimplicitePire2.solution(texte, ordre));
+        System.out.println(SimplicitePire3.solution(texte, ordre));
         // output: [classer, texte, exemple, a, de]
     }
 }
